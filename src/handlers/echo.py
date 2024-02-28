5a0c18857dbd9a1b8d1f806af8de25603c33801c
YASHALAWA8 <95289183+YASHALAWA8@users.noreply.github.com>
2024-02-28 14:39:34 +0300

    keyboard 1

--- src/handlers/echo.py
+++ src/handlers/echo.py
@@ -2,19 +2,22 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 from src.models.users.dao import add_user
-
+from src.keyboards.keyboards import keyboard
 
 router: Router = Router()
 
 @router.message(Command(commands=["start"]))
-async def handle_message(message: Message):
+async def start(message: Message):
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
     username = message.from_user.username
     user_id = message.from_user.id
     await add_user(first_name, last_name, username, user_id)
-    await message.reply('Thanks for your message!')
+    await message.reply(
+        text='Спасибо мы тебя внесли в список пидарасов, отправим этот список презеденту',
+        reply_markup=keyboard
+    )
 
 @router.message()
-async def process_any_message(message: Message):
+async def echo_massege(message: Message):
     await message.reply(text=message.text)
--- src/keyboards/keyboards.py
+++ src/keyboards/keyboards.py
@@ -1,1 +1,13 @@
 # Create your keyboards here.
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+
+button_1 = KeyboardButton(text='Собак 🦮')
+button_2 = KeyboardButton(text='Огурцов 🥒')
+
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[[button_1, button_2]],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
