2dcc0857e12575a9feae86428224786e05097355
YASHALAWA8 <95289183+YASHALAWA8@users.noreply.github.com>
2024-02-28 13:59:09 +0300

    hendler start and echo

--- src/handlers/echo.py
+++ src/handlers/echo.py
@@ -1,20 +1,20 @@
 from aiogram import Router
 from aiogram.types import Message
-
-from src.models.users.router import add_user
+from aiogram.filters import Command
+from src.models.users.dao import add_user
 
 
 router: Router = Router()
 
-
-# @router.message()
-# async def process_any_message(message: Message):
-#     await message.reply(text=message.text)
-
-@router.message()
+@router.message(Command(commands=["start"]))
 async def handle_message(message: Message):
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
     username = message.from_user.username
-    await add_user(first_name, last_name, username)
+    user_id = message.from_user.id
+    await add_user(first_name, last_name, username, user_id)
     await message.reply('Thanks for your message!')
+
+@router.message()
+async def process_any_message(message: Message):
+    await message.reply(text=message.text)
--- src/models/users/models.py
+++ src/models/users/models.py
@@ -6,6 +6,7 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, unique=True)
     first_name = Column(String)
     last_name = Column(String)
     username = Column(String)
